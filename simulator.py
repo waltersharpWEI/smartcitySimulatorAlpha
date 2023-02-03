a3361fdf8a3b87eeb1ca94ee70e5d116ea98f5d3
waltersharpWEI <waltersharp@163.com>
2023-02-04 07:37:29 +0800

    server only latency based optimizer

--- simulator.py
+++ simulator.py
@@ -18,11 +18,21 @@ class Simulator():
                 max_acc = row['1st_acc']
         return max_acc
 
+    def opt_acc_latency_server_only(self, bd):
+        max_acc = 0
+        latency_bound = 1000 / self.fps
+        for index, row in self.df_t.iterrows():
+            total_latency = row['size'] / bd * 1000 + row['1st_inf'] \
+                            + row['encoding_latency'] + row['decoding_latency']
+            if  total_latency<latency_bound and row['1st_acc'] > max_acc:
+                max_acc = row['1st_acc']
+        return max_acc
+
     def sim_acc(self):
         results = []
         bds = self.df_bd['bandwidth']
         for bd in bds:
-            acc = self.opt_acc(bd)
+            acc = self.opt_acc_latency_server_only(bd)
             log.info("bandwidth: "+str(bd))
             log.info("accuracy: "+str(acc))
             results.append(acc)
